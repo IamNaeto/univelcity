a20d270405cc9c711f836bc878418514068c93a4
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-06-03 00:04:02 +0100

    created and read data from database

--- crud-operation/index.js
+++ crud-operation/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const productScheme = require("./models/productModel.js");
 
 app = express(); //express instance
+app.use(express.json()); //registering express json middleware
+app.use(express.urlencoded({ extended: false })); // registering urlencoded to express which helps us to post using a form or urlencoded format
 
 const PORT = process.env.PORT || 3000; //port number
 
@@ -17,3 +20,36 @@ mongoose //connection to mongodb
   .catch(() => {
     console.log(`Connection to database failed`);
   });
+
+// get method
+app.get("/", (req, res) => {
+  res
+    .send({
+      msg: "Welcome to the home page",
+      hint: "More is coming",
+    })
+    .status(200);
+});
+
+// post method
+app.post("/api/product/list/create", async (req, res) => {
+  if (
+    !req.body.product_name ||
+    !req.body.product_qty ||
+    !req.body.product_price
+  ) {
+    res.status(401).send({
+      msg: "All fields must be field",
+    });
+  }
+
+  try {
+    const productDetails = await productScheme.create(req.body);
+    res.status(201).send(productDetails);
+  } catch (error) {
+    res.status(500).send({
+      msg: "Internal Server Error",
+      error: error.message,
+    });
+  }
+});
