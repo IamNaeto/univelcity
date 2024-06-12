9d7805125345ad11cd58afc6480c134c015089a4
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-06-12 09:04:10 -0400

    frontend/feat/installations && setups

---
+++ product-store/frontend/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-center font-extrabold text-green-900">
+        Welcome To My App
+      </h1>
+    </>
+  );
+};
+
+export default Home;
