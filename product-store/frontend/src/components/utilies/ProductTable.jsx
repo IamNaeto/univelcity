5cf146bcf34a1f52ff3a4b0ab4f3cbb059bb96c3
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-06-12 18:59:45 -0400

    fetch product data from the backend

---
+++ product-store/frontend/src/components/utilies/ProductTable.jsx
@@ -0,0 +1,19 @@
+const ProductTable = () => {
+    return ( 
+        <table className="w-full border-separate border-spacing-1">
+            <thead>
+                <th className="border border-slate-600 rounded-md">No</th>
+                <th className="border border-slate-600 rounded-md">Product Name</th>
+                <th className="border border-slate-600 rounded-md">Product Name</th>
+                <th className="border border-slate-600 rounded-md">Product Quantity</th>
+                <th className="border border-slate-600 rounded-md">Product Price</th>
+                <th className="border border-slate-600 rounded-md">Actions</th>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+     );
+}
+ 
+export default ProductTable;
--- product-store/frontend/src/pages/home.jsx
+++ product-store/frontend/src/pages/home.jsx
@@ -1,12 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { MdAddBox } from "react-icons/md";
+import axios from "axios";
+import { Toaster, toast } from "sonner";
+import ProductCard from "../components/utilies/ProductCard";
+import ProductTable from "../components/utilies/ProductTable";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("table");
+
+  const toggleTable = () => {
+    setShowType("table");
+  };
+
+  const toggleCard = () => {
+    setShowType("card");
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("http://localhost:3000/api/products");
+      const data = response.data
+      setData(data);
+      console.log(data);
+      toast.success("Data fetched successfully");
+    } catch (error) {
+      console.error(error);
+      toast.error("Error: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <h1 className="text-4xl text-center font-extrabold text-green-900">
-        Welcome To My App
-      </h1>
-    </>
+    <div className="px-20 py-4">
+      <Toaster position="top-center" richColors/>
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          className="bg-sky-600 hover:bg-sky-800 px-4 py-1 rounded-md text-white font-bold transition-all delay-150"
+          onClick={toggleTable}
+        >
+          Table
+        </button>
+        <button
+          className="bg-sky-600 hover:bg-sky-800 px-4 py-1 rounded-md text-white font-bold transition-all delay-150"
+          onClick={toggleCard}
+        >
+          Card
+        </button>
+      </div>
+
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl text-sky-800 my-8 font-bold">PRODUCT LIST</h1>
+        <MdAddBox className="text-sky-800 text-4xl cursor-pointer" />
+      </div>
+
+      <div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : showType === "table" ? (
+          <ProductTable data={data} />
+        ) : (
+          <ProductCard data={data} />
+        )}
+      </div>
+    </div>
   );
 };
 
