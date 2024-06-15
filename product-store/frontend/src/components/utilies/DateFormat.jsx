1c4cec7f8f46cdb0f54d3f22b6d4aaa8a89ef0a4
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-06-15 11:18:04 -0400

    frontend/feat/adjust product detail stylings

---
+++ product-store/frontend/src/components/utilies/DateFormat.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DateFormat = ({ dateString }) => {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
+
+  return <span>{formatDate(dateString)}</span>;
+};
+
+export default DateFormat;
--- product-store/frontend/src/pages/ProductDetail.jsx
+++ product-store/frontend/src/pages/ProductDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import PropagateLoader from "react-spinners/PropagateLoader";
+import DateFormat from "../components/utilies/DateFormat";
 import { BiSolidError } from "react-icons/bi";
 import BackButton from "../components/utilies/BackButton";
 import { useNavigate } from "react-router-dom";
@@ -31,14 +32,14 @@ const ProductDetails = () => {
 
   if (loading)
     return (
-      <div className="flex flex-col items-center justify-center gap-4 p-10 text-3xl text-sky-800 min-h-screen">
+      <div className="bg-[#f1f5fd] flex flex-col items-center justify-center gap-4 p-10 text-3xl text-sky-800 min-h-screen">
         <p>Loading...</p> <PropagateLoader color="#075985" />
       </div>
     );
 
   if (!product)
     return (
-      <div className="flex items-center justify-center p-10 text-3xl  text-red-900 min-h-screen">
+      <div className="bg-[#f1f5fd] flex items-center justify-center p-10 text-3xl  text-red-900 min-h-screen">
         {" "}
         <BiSolidError className="text-6xl" />
         <p>404! Product not found</p>
@@ -57,7 +58,7 @@ const ProductDetails = () => {
           alt="product-img"
         />
       </div>
-      <div className="text-2xl text-white text-left flex flex-col gap-3">
+      <div className="text-lg text-white text-left flex flex-col gap-2">
         <h1>
           <b>Name:</b> {product.product_name}
         </h1>
@@ -88,21 +89,21 @@ const ProductDetails = () => {
         </p>
 
         <p>
-          <b>Date Created: </b> {product.createdAt}
+          <b>Date Created: </b> <DateFormat dateString={product.createdAt}/>
         </p>
 
         <p>
-          <b>Date Updated: </b> {product.updatedAt}
+          <b>Date Updated: </b> <DateFormat dateString={product.updatedAt}/>
         </p>
 
         <div className="mt-4 flex text-xl items-center gap-4">
           <button
-            className="text-white border bg-yellow-600 hover:bg-yellow-800 rounded-full px-6 py-4 transition-all delay-150"
+            className="text-white font-semibold border bg-yellow-600 hover:bg-yellow-800 shadow shadow-white hover:shadow-none rounded-full px-4 py-2 file:transition-all delay-150"
             onClick={() => handleProductUpdate(product._id)}
           >
             Edit Product
           </button>
-          <button className="text-white border bg-red-600 hover:bg-red-800 rounded-full px-6 py-4 transition-all delay-150">
+          <button className="text-white font-semibold border bg-red-600 hover:bg-red-800 shadow shadow-white hover:shadow-none rounded-full px-4 py-2 transition-all delay-150">
             Delete Product
           </button>
         </div>
