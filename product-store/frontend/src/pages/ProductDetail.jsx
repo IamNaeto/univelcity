93c05fa18e570813a1cd8e77487963ae6898c626
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-06-16 13:05:34 -0400

    frontend/feat/implement and consume delete product endpoint

--- product-store/frontend/src/pages/ProductDetail.jsx
+++ product-store/frontend/src/pages/ProductDetail.jsx
@@ -6,12 +6,18 @@ import DateFormat from "../components/utilies/DateFormat";
 import { BiSolidError } from "react-icons/bi";
 import BackButton from "../components/utilies/BackButton";
 import { useNavigate } from "react-router-dom";
+import { Toaster, toast } from "sonner";
+import Modal from 'react-modal';
 
 const ProductDetails = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const navigate = useNavigate();
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -46,11 +52,51 @@ const ProductDetails = () => {
       </div>
     );
 
+  const handleProductDelete = async () => {
+    try {
+      const request = await axios.delete(`${import.meta.env.VITE_APP_PRODUCT_ROUTE_URL}/delete/${id}`);
+      console.log(request);
+      toast.success(`${product.product_name} deleted successfully`);
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    } catch (error) {
+      console.log("Error", error);
+      toast.error(error.message);
+    }
+  };
+
+  const confirmDelete = () => {
+    handleProductDelete();
+    closeModal();
+  };
+
   const handleProductUpdate = (id) => {
     navigate(`/product/update/${id}`);
   };
+
   return (
     <div className="min-h-screen grid grid-cols-1 xl:grid-cols-2 items-center gap-6 px-10 py-4 border bg-gray-800 rounded-md">
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Confirm Delete"
+        ariaHideApp={false}
+        className="flex items-center justify-center min-h-screen"
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm"
+      >
+        <div className="flex flex-col items-center justify-center gap-4 bg-white p-8 rounded-lg shadow-lg max-w-md mx-auto text-center">
+          <BiSolidError className=" text-center text-5xl text-red-500"/>
+          <h2 className="text-xl font-bold">Are you sure you want to delete <br /> <span className="text-green-600 font-bold">{product.product_name}?</span></h2>
+          <p className="text-red-600 font-bold">This action cannot be reversed!</p>
+          <div className="flex gap-6">
+            <button onClick={confirmDelete} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">Yes</button>
+            <button onClick={closeModal} className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">No</button>
+          </div>
+        </div>
+      </Modal>
+      
       <div>
         <img
           className="w-full rounded-xl"
@@ -103,12 +149,17 @@ const ProductDetails = () => {
           >
             Edit Product
           </button>
-          <button className="text-white font-semibold border bg-red-600 hover:bg-red-800 shadow shadow-white hover:shadow-none rounded-full px-4 py-2 transition-all delay-150">
+          <button 
+          className="text-white font-semibold border bg-red-600 hover:bg-red-800 shadow shadow-white hover:shadow-none rounded-full px-4 py-2 transition-all delay-150"
+          onClick={openModal}
+          >
             Delete Product
           </button>
         </div>
 
         <BackButton route={"/"} />
+
+        <Toaster position="top-right" richColors/>
       </div>
     </div>
   );
